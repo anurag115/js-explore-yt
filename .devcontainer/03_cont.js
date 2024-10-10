00e2dd05875ae7d3eab54006f9f40c7ec19a9736
Anurag <115284047+anurag115@users.noreply.github.com>
2024-10-10 22:20:35 +0000

    convertion of datatype

---
+++ .devcontainer/03_cont.js
@@ -0,0 +1,24 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33 
+// "33abc" => Nan
+// true =>1; false =>0
+// "" => false
+// "hitesh"=> true
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn =  Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
